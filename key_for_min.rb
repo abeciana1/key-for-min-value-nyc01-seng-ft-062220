51ee267b9ed8a88f8a118093799e2007d544604e
Alex Beciana <alex.beciana@gmail.com>
2020-06-14 01:50:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  return nil if name_hash.length == 0
+  return nil if name_hash.size == 0
   
   minv = 0 
   mink = 0
